5ec8a3b6faa096d23e779be0dbf036374e97d43f
masterahmedhussain <masterahmedhussain@gmail.com>
2021-04-16 00:57:36 +0530

    added to check wheather the input field is empty or not

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const todoInput = document.querySelector(".form-input");
+let todoInput = document.querySelector(".form-input");
 const todoButton = document.querySelector(".form-submit");
 const todoList = document.querySelector(".list")
 
@@ -13,31 +13,42 @@ function addTodo(event) {
     //prevent page from reloading
     event.preventDefault();
     //making a new div
-    const todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo");
-
+    
+    //Checking the input field
+    
+    
+    
     // making a new list 
     const newTodo = document.createElement("li");
-    newTodo.innerText = todoInput.value;
-    newTodo.classList.add('todo-item');
-    // append the child into the div 
-    todoDiv.appendChild(newTodo);
-
-    const completedButton = document.createElement("button");
-    completedButton.innerHTML = '<i class="fas fa-check"></i>';
-    completedButton.classList.add("completed-btn");
-    todoDiv.appendChild(completedButton);
-
-
-    const trashButton = document.createElement("button");
-    trashButton.innerHTML = "<i class='fas fa-trash'></i>";
-    trashButton.classList.add("trashed-btn");
-    todoDiv.appendChild(trashButton);
-
-    todoList.appendChild(todoDiv);
-
-    //clear TodoInput value
-    todoInput.value = ""
+    if (todoInput.value === ""){
+      alert("You cannot add the empty TASK");
+      
+    }else{
+      const todoDiv = document.createElement("div");
+      todoDiv.classList.add("todo");
+      
+      newTodo.innerText = todoInput.value;
+      newTodo.classList.add('todo-item');
+      // append the child into the div 
+      todoDiv.appendChild(newTodo);
+      
+      const completedButton = document.createElement("button");
+      completedButton.innerHTML = '<i class="fas fa-check"></i>';
+      completedButton.classList.add("completed-btn");
+      todoDiv.appendChild(completedButton);
+      
+      
+      const trashButton = document.createElement("button");
+      trashButton.innerHTML = "<i class='fas fa-trash'></i>";
+      trashButton.classList.add("trashed-btn");
+      todoDiv.appendChild(trashButton);
+      todoList.appendChild(todoDiv);
+      }
+      
+    
+      
+      //clear TodoInput value
+      todoInput.value = ""
 
 }
 
